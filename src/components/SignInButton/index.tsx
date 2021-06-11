6984121a245e698237f3b39d0af5e78bc5b101b0
Marcos Koch <marcos_koch@hotmail.com>
2021-06-11 14:48:05 -0300

    feat: login with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,25 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
+
+  console.log(session)
   
-  return isUserLoggedIn ? (
+  return session ? (
     <button className={styles.signInButton} type="button">
       <FaGithub color="#04d361" />
       Marcos Koch
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button 
+      className={styles.signInButton} 
+      type="button"
+      onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
