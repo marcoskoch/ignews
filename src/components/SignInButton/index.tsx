c86a3f9692cbf3774db157dc6a29359b5debc383
Marcos Koch <marcos_koch@hotmail.com>
2021-06-15 17:43:55 -0300

    feat: create user in faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,17 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, useSession, signOut } from 'next-auth/client';
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
   const [session] = useSession()
-
-  console.log(session)
   
   return session ? (
-    <button className={styles.signInButton} type="button">
+    <button 
+      className={styles.signInButton} 
+      type="button"
+      onClick={() => signOut()}>
       <FaGithub color="#04d361" />
       Marcos Koch
       <FiX color="#737380" className={styles.closeIcon} />
